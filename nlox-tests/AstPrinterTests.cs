746810020d9760bfc085ece99cd3b7f7e662b47d
Henrik Andersson <h@hnra.dev>
2022-08-15 19:52:28 +0200

    Added AstPrinter and some tests

---
+++ nlox-tests/AstPrinterTests.cs
@@ -0,0 +1,54 @@
+namespace nlox_tests;
+
+using CraftingInterpreters;
+using CraftingInterpreters.AstGen;
+
+public class AstPrinterTests
+{
+    static readonly AstPrinter astPrinter = new AstPrinter();
+
+    [Test]
+    public void TestSimpleBinaryExpr()
+    {
+        var binaryExpr = new Binary(
+            new Literal("left"),
+            new Token(TokenType.AND, "and", null, 0),
+            new Literal("right"));
+
+        Assert.AreEqual("(and left right)", astPrinter.Print(binaryExpr));
+    }
+
+    [Test]
+    public void TestSimpleUnaryExpr()
+    {
+        var unaryExpr = new Unary(
+            new Token(TokenType.BANG, "!", null, 0),
+            new Literal("right"));
+
+        Assert.AreEqual("(! right)", astPrinter.Print(unaryExpr));
+    }
+
+    [Test]
+    public void TestNestledExpr()
+    {
+        var unaryExpr = new Unary(
+            new Token(TokenType.BANG, "!", null, 0),
+            new Literal("right"));
+
+        var binaryExpr = new Binary(
+            unaryExpr,
+            new Token(TokenType.AND, "and", null, 0),
+            new Literal("right"));
+
+        Assert.AreEqual("(and (! right) right)", astPrinter.Print(binaryExpr));
+    }
+
+    [Test]
+    public void TestGroupingExpr()
+    {
+        var grouping = new Grouping(
+            new Literal("right"));
+
+        Assert.AreEqual("(group right)", astPrinter.Print(grouping));
+    }
+}
---
+++ nlox/AstPrinter.cs
@@ -0,0 +1,34 @@
+namespace CraftingInterpreters;
+
+using AstGen;
+
+public class AstPrinter : Visitor<string>
+{
+    public string Print(Expr expr) => expr.Accept(this);
+
+    public string VisitBinaryExpr(Binary expr) =>
+        this.Parenthesize(expr.op.lexeme, expr.left, expr.right);
+
+    public string VisitGroupingExpr(Grouping expr) =>
+        this.Parenthesize("group", expr.expression);
+
+    public string VisitLiteralExpr(Literal expr) =>
+        expr.value.ToString() ?? "";
+
+    public string VisitUnaryExpr(Unary expr) =>
+        this.Parenthesize(expr.op.lexeme, expr.right);
+
+    private string Parenthesize(string name, params Expr[] exprs)
+    {
+        var sw = new StringWriter();
+
+        sw.Write($"({name}");
+        foreach (var expr in exprs)
+        {
+            sw.Write($" {expr.Accept(this)}");
+        }
+        sw.Write(")");
+
+        return sw.ToString();
+    }
+}

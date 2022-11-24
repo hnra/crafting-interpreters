e29bb6bbc96a38e745ba63bf1b091eec367efe68
Henrik Andersson <h@hnra.dev>
2022-11-24 20:15:14 +0100

    Disallow usage of this outside of classes

--- clox/src/compiler.c
+++ clox/src/compiler.c
@@ -67,8 +67,13 @@ typedef struct Compiler {
     int scopeDepth;
 } Compiler;
 
+typedef struct ClassCompiler {
+    struct ClassCompiler* enclosing;
+} ClassCompiler;
+
 Parser parser;
 Compiler* current = NULL;
+ClassCompiler* currentClass = NULL;
 
 static Chunk* currentChunk() { return &current->function->chunk; }
 
@@ -474,7 +479,14 @@ static void namedVariable(Token name, bool canAssign) {
 
 static void variable(bool canAssign) { namedVariable(parser.previous, canAssign); }
 
-static void this_(bool canAssign) { variable(false); }
+static void this_(bool canAssign) {
+    if (currentClass == NULL) {
+        error("Can't use 'this' outside of a class.");
+        return;
+    }
+
+    variable(false);
+}
 
 static void unary(bool canAssign) {
     TokenType operatorType = parser.previous.type;
@@ -631,6 +643,10 @@ static void classDeclaration() {
     emitBytes(OP_CLASS, nameConstant);
     defineVariable(nameConstant);
 
+    ClassCompiler classCompiler;
+    classCompiler.enclosing = currentClass;
+    currentClass = &classCompiler;
+
     namedVariable(className, false);
     consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
 
@@ -640,6 +656,8 @@ static void classDeclaration() {
 
     consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
     emitByte(OP_POP);
+
+    currentClass = currentClass->enclosing;
 }
 
 static void funDeclaration() {

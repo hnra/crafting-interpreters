063bedf393950698d23aa60ba3b63e531176eebe
Henrik Andersson <h@hnra.dev>
2022-11-26 10:26:37 +0100

    Added class inheritance

--- clox/src/debug.c
+++ clox/src/debug.c
@@ -147,6 +147,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return constantInstruction("OP_METHOD", chunk, offset);
         case OP_INVOKE:
             return invokeInstruction("OP_INVOKE", chunk, offset);
+        case OP_INHERIT:
+            return simpleInstruction("OP_INHERIT", offset);
         default:
             printf("Unknown opcode %d\n", instruction);
             return offset + 1;

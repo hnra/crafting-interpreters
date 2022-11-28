31bccfd09b328e61eac18f9925bc247968ecccc7
Henrik Andersson <h@hnra.dev>
2022-11-28 21:18:30 +0100

    Improve benchmark by ~40% by avoiding modulo

--- clox/src/table.c
+++ clox/src/table.c
@@ -19,7 +19,7 @@ void freeTable(Table* table) {
 }
 
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
-    uint32_t index = key->hash % capacity;
+    uint32_t index = key->hash & (capacity - 1);
     Entry* tombstone = NULL;
     for (;;) {
         Entry* entry = &entries[index];
@@ -32,7 +32,7 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
         } else if (entry->key == key) {
             return entry;
         }
-        index = (index + 1) % capacity;
+        index = (index + 1) & (capacity - 1);
     }
 }
 
@@ -106,7 +106,7 @@ void tableAddAll(Table* from, Table* to) {
 ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t hash) {
     if (table->count == 0) return NULL;
 
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & (table->capacity - 1);
     for (;;) {
         Entry* entry = &table->entries[index];
         if (entry->key == NULL) {
@@ -115,7 +115,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t
                    memcmp(entry->key->chars, chars, length) == 0) {
             return entry->key;
         }
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1);
     }
 }
 
